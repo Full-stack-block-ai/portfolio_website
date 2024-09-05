abe93b14950db7c765348c754a7967ba0aa89931
Steve @fullstack <Stephenjmhough@outlook.com>
2024-09-05 16:26:52 +0100

    completed the initial build of the html, css and added javascript functionality

--- resources/script.js
+++ resources/script.js
@@ -1,17 +1,21 @@
-// Select all toggle buttons
+// Toggle project card expansion
 document.querySelectorAll('.btn-project-toggle').forEach(button => {
     button.addEventListener('click', function() {
-        const card = this.parentElement; // Get the parent card
-        const isExpanded = card.getAttribute('data-expanded') === 'true'; // Check if the card is expanded
+         // Get the parent card and check if the card is expanded
+        const card = this.parentElement;
+        const isExpanded = card.getAttribute('data-expanded') === 'true'; 
 
         if (isExpanded) {
             // Collapse the card
             card.setAttribute('data-expanded', 'false');
-            this.innerText = 'View More'; // Change button text
+            this.innerText = 'View More';
         } else {
             // Expand the card
             card.setAttribute('data-expanded', 'true');
-            this.innerText = 'View Less'; // Change button text
+            this.innerText = 'View Less'; 
         }
     });
 });
+
+// Set the current year in the footer
+document.getElementById('current-year').textContent = new Date().getFullYear();
